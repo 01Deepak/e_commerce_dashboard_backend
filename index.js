a21c3a7d41dd3cb53ec7d79ce0b59be102119776
Deepak <deepak.singh@networkon.io>
2024-08-01 00:26:04 +0530

    fixed cors issue

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 require('./db/config');
+const cors = require('cors');
 const User = require('./db/User');
 
 const app = express();
 app.use(express.json());
+app.use(cors());
+
 
  app.post('/register', (req, res) => {
     const user = new User(req.body);
