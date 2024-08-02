7266bfac6b4031fa787cfbcf6d66a8d0ee0f8f14
Deepak <deepak.singh@networkon.io>
2024-08-02 09:22:43 +0530

    create login api

--- index.js
+++ index.js
@@ -8,10 +8,24 @@ app.use(express.json());
 app.use(cors());
 
 
- app.post('/register', (req, res) => {
+ app.post('/register', async (req, res) => {
     const user = new User(req.body);
-    user.save();
-    res.send(user);
+    let result = await user.save();
+    result = result.toObject();
+    delete result.password;
+    res.send(result);
+})
+
+app.post('/login', async (req, res) => {
+    const user = await User.findOne(req.body).select('-password');
+    if (req.body.password && req.body.email) {
+        if (user) {
+            return res.send(user);
+        }
+        return res.send("user not found");
+    }else{
+        return res.send("user not found");
+    }
 })
 
 app.listen(4000);
